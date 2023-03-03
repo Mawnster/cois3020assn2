dbfcc821617fd922206a7b1be40ddf85da2a53d3
mchlleliu <115041979+mchlleliu@users.noreply.github.com>
2023-03-03 14:50:21 -0500

    edit comments

--- AugmentedSkipList.cs
+++ AugmentedSkipList.cs
@@ -1,3 +1,7 @@
+// COIS 3020H Assignment 2 Part B
+// Augmented Skip List to support Rank operations
+// Anything modified from the source code will have a bunch of comment out ////
+// and *** UPDATE *** preceeding the comment
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,7 +21,6 @@ namespace SkipLists
         void Insert(T item);     // Inserts item into the skip list (duplicates are permitted)
         bool Contains(T item);   // Returns true if item is found; false otherwise
         void Remove(T item);     // Removes one occurrence of item (if possible) from the skip list
-
         // Augmented methods
         T RankI(int i);          // Return the item with the given rank
         int RankII(T item);       // Return the rank of a given item
@@ -30,8 +33,7 @@ namespace SkipLists
         private Node head;          // Header node of height 32
         private int maxHeight;     // Maximum height among non-header nodes
         private Random rand;          // For generating random heights
-        // *** UPDATE (just for testing) ***
-        public int size { get; set; }
+        public int size { get; set; } // *** UPDATE (just for testing) ***
 
         // Class Node (used by SkipList)
 
@@ -51,11 +53,6 @@ namespace SkipLists
                 Height = height;
                 Next = new Node[Height];
                 Length = new int[Height];
-                for (int i = 0; i < Height; i++)
-                {
-                    Next[i] = null;
-                    Length[i] = 0;
-                }
             }
         }
 
@@ -72,10 +69,10 @@ namespace SkipLists
         }
 
 
-        //Public Insert
+        //Public Insert ** UPDATED to recursion**
         //Initiate random height and inserts the given item into the skip list
         //Duplicate items are permitted
-        //Expected time complexity: O(log n)
+        //Expected time complexity: O(1)
 
         public void Insert(T item)
         {
@@ -88,47 +85,39 @@ namespace SkipLists
                 R >>= 1;    // Right shift one bit
             }
             if (height == maxHeight) maxHeight++;
-
-
             // Create and insert node recursively
             Node newNode = new Node(item, height + 1);
-
-            //  *** UPDATE *** 
-            // pos keeps track of the current node's position
-            // floor starts from the maxHeight
+            // pos keeps track of the current node's position, floor starts from the maxHeight
             int pos = 0, floor = maxHeight - 1;
-            Stack<int> steps = new Stack<int>(); // second stack acts like a queue to store the number of pointers from the first floor
             Node cur = head;
             // recursive insert call
-            Insert(cur, newNode, ref pos, floor, steps);
-
+            Insert(cur, newNode, ref pos, floor);
             size++; // update the count of items in the skip list
         }
 
-        // *** UPDATE ***
-        // Private Insert
+        ///// *** UPDATE ***
+        // Private Insert 
         // Performs post-order traversal to insert given item to skip list and update lengths for each pointer
-        private void Insert(Node cur, Node newNode, ref int pos, int floor, Stack<int> steps)
+        // Expected time complexity: O(log n)
+        
+        private void Insert(Node cur, Node newNode, ref int pos, int floor)
         {
             if (floor >= 0)
             {
-                int stepCount = 0; // restart count for steps (arrows) for each floor
-
-                // find the insert position from the top
+                int stepCount = 0; // restart count for nodes traversed for each floor
+                // traverse current floor
                 while (cur.Next[floor] != null && cur.Next[floor].Item.CompareTo(newNode.Item) < 0)
                 {
+                    // update step count whenever we move to the right on the same floor
                     stepCount += cur.Length[floor];
                     cur = cur.Next[floor];
                 }
-                // if you go down a floor, insertion is going to happen somewhere after the current node
-                // if there is a node after the current one, then the length of the pointer increases by one from the insertion
+                // updating lengths at floors higher than the newNode's height
                 if(floor >= newNode.Height && cur.Next[floor] != null)
                     cur.Length[floor] += 1;
-                steps.Push(stepCount); // push the distance of the current level into the stack
-
-                // go down the floor of the previous node before insertion 
-                Insert(cur, newNode, ref pos, floor - 1, steps);
-
+                // go down to the next floor
+                Insert(cur, newNode, ref pos, floor - 1);
+                // break edges and connect to inserted newNode
                 if (floor < newNode.Height)
                 {
                     // adjust the pointers for each floor
@@ -137,13 +126,11 @@ namespace SkipLists
                     // update number of nodes skipped for the new node and the node before the insertion
                     newNode.Length[floor] = cur.Length[floor] - pos; 
                     cur.Length[floor] = pos + 1;
-                    // update the position counter using the steps taken at each floor (in reverse order)
-                    // e.g., if we're at floor 0, then steps's dequeue is the steps at maxHeight - 1
-                    pos += steps.Pop(); 
+                    // update the position counter using the steps taken at each floor
+                    pos += stepCount; 
                 }
             }
         }
-
 
         // Contains
         // Returns true if the given item is found in the skip list; false otherwise
@@ -163,26 +150,25 @@ namespace SkipLists
             return false;
         }
 
-        // Public Remove (post-order recursion)
-        // Removes one occurrence (if possible) of the given item from the skip list
-        // Expected time complexity: O(log n)
+        // Public Remove (updated to post-order recursion)
+        // Calls private Remove method to remove one occurrence (if possible) of the given item from the skip list
+        // Expected time complexity: O(1)
 
         public void Remove(T item)
         {
             Node cur = head;
-            int floor = maxHeight - 1;
-            int height = int.MaxValue;
-
+            int floor = maxHeight - 1; // start from the top floor
+            int height = int.MaxValue; // intiate height as maxValue
             if (Remove(item, cur, floor, ref height) != null)
-                size -= 1; // update the size of the skip list if removal is successful
+                size -= 1; //only update the size of the skip list if removal is successful
         }
-
-
+        
+        ///// *** UPDATE ***
         // Private Remove 
-        // Traverse to the node to be removed.
+        // Traverse to the node to be removed, then remove node on the way back
         // Post order recursion is done so that the height of the node to be removed can be first determined
-        // The height is used in order to maintain the lengths of the pointers
-
+        // Expected time complexity: O(log n)
+        
         private Node Remove(T item, Node cur, int floor, ref int height)
         {
             Node removed = null;
@@ -193,8 +179,6 @@ namespace SkipLists
                     cur = cur.Next[floor];
                 // recursive call. Travel to the floor 0 from maxHeight - 1 before processing
                 Remove(item, cur, floor - 1, ref height);
-
-
                 // Processing removal for each floor starting from 0
                 if(cur.Next[floor] != null && cur.Next[floor].Item.CompareTo(item) == 0)
                 {
@@ -204,18 +188,15 @@ namespace SkipLists
                     cur.Length[floor] += cur.Next[floor].Length[floor] - 1; // update length of pointer
                     cur.Next[floor] = cur.Next[floor].Next[floor];    // Cut off reference at level i
                 }
-
                 // decrease length by one for floors over the height of the node removed
                 if (floor >= height && cur.Next[floor] != null)
                    cur.Length[floor] -= 1;
-
                 // Decrease maximum height by 1 when the number of nodes at height i is 0
                 if (head.Next[floor] == null)
                     maxHeight--;
             }
             return removed;
         }
-
 
         // Print
         // Outputs the item in sorted order
@@ -239,7 +220,6 @@ namespace SkipLists
         public void Profile()
         {
             Node cur = head;
-
             while (cur != null)
             {
                 Console.WriteLine(new string('*', cur.Height));  // Outputs a string of *s
@@ -247,41 +227,31 @@ namespace SkipLists
             }
         }
 
-        // Public Rank I
-        // Calls private Rank which returns the item with rank i
+        // Rank I
+        // Returns the item with the given rank i
         // Expected time complexity:  O(log n)
 
         public T RankI(int i)
         {
-            return RankI(head, i);
-        }
-
-        // Private Rank I
-        // Returns the item with the given rank i
-        // Expected time complexity:  O(log n)
-
-        private T RankI(Node p, int i)
-        {
-            int r = 0;
-            if (i > 0 && i <= size)                  // Check for correct input
+            int curRank = 0;
+            Node cur = head;
+            if (i > 0 && i <= size)  // Check for correct input
             {
                 for (int x = maxHeight - 1; x >= 0; x--)  // for each level
-                {
-                    // traverse to the given rank
-                    while (p.Next[x] != null && (r + p.Length[x]) <= i)
+                { 
+                    while (cur.Next[x] != null && (curRank + cur.Length[x]) <= i) // traverse current floor
                     {
-                        r += p.Length[x]; // traverse and update the current rank
-                        p = p.Next[x];
+                        curRank += cur.Length[x]; // update the current rank as we traverse nodes
+                        cur = cur.Next[x];
                     }
                 }
-                return p.Item;
+                return cur.Item;
             }
-            else
-                // i out of range
+            else   // if i out of range
                 return default(T);
         }
 
-
+        /// *** UPDATE ***
         // Rank II
         // Calculates and returns the rank of the given item if found; -1 otherwise
         // Expected time complexity:  O(log n)
@@ -289,21 +259,19 @@ namespace SkipLists
         public int RankII(T item)
         {
             Node cur = head;
-            int r = 0;
+            int curRank = 0;
             for (int i = maxHeight - 1; i >= 0; i--)  // for each level
             {
-                while (cur.Next[i] != null && cur.Next[i].Item.CompareTo(item) < 0)
+                while (cur.Next[i] != null && cur.Next[i].Item.CompareTo(item) < 0) // traverse current floor
                 {
-                    r += cur.Length[i]; // count total lengths traversed
+                    curRank += cur.Length[i]; // count up total lengths of nodes traversed
                     cur = cur.Next[i];
                 }
                 if (cur.Next[i] != null && cur.Next[i].Item.CompareTo(item) == 0)
-                    return r += cur.Length[i];
+                    return curRank += cur.Length[i]; // return current calculated rank
             }
             return -1;              // Return -1 if item is not found
-
         }
-
     }
 
     class Program
@@ -311,32 +279,27 @@ namespace SkipLists
         static void Main(string[] args)
         {
             Console.WriteLine("Initiating Skip List...");
-
             SkipList<int> S = new SkipList<int>();
-
+            //// original testing
             //int i;
             ////for (i = 1; i <= 10; i++) { S.Insert(i);}
             //for (i = 1; i <= 6; i++) { S.Insert(i); S.Insert(7 - i); }
-
             //S.Print();
             //S.Profile();
-            //for(i = 1; i <= 6; i++)
+            //for (i = 1; i <= 6; i++)
             //    Console.WriteLine("Rank of " + i + "        : " + S.RankII(i));
             //Console.WriteLine("Min and max      : " + S.RankI(1) + " " + S.RankI(S.size));
-
             //for (i = 1; i <= 2; i++) { S.Remove(6); S.Remove(3); }
             //for (i = 1; i <= 6; i++) Console.WriteLine(S.Contains(i));
-
             //for (i = 1; i <= 6; i++)
             //    Console.WriteLine("Rank of " + i + "        : " + S.RankII(i));
             //Console.WriteLine("Min and max        : " + S.RankI(1) + " " + S.RankI(S.size));
             //S.Print();
             //S.Profile();
-
             //Console.ReadKey();
 
+            // user-menu
             System.Threading.Thread.Sleep(100);
-
             bool run = true;
             while (run == true) //repeat until quit
             {
@@ -350,11 +313,9 @@ namespace SkipLists
                 "\nFind Rank of item (Rank II)     (R):" +
                 "\nQuit                            (Q):" +
                 "\nInput? :");
-
                 bool valid = false; //validation
                 char input = default;
                 int item;
-
                 while (valid == false)
                 {
                     try
@@ -368,7 +329,6 @@ namespace SkipLists
                         Console.Write("Input :");
                     }
                 }
-
                 switch (input) //operation based on menu input
                 {
                     // initiate default skip list
@@ -376,10 +336,8 @@ namespace SkipLists
                         Console.WriteLine("Initializing default Skip List ... ");
                         System.Threading.Thread.Sleep(100);
                         for (int i = 10; i <= 60; i++) { S.Insert(i); S.Insert(70 - i); i += 9; }
-                        S.Print();
-                        S.Profile();
+                        S.Print(); S.Profile();
                         break;
-
                     // call Insert()
                     case 'N':
                         Console.Write("Please enter the item you would like to insert >> ");
@@ -387,13 +345,8 @@ namespace SkipLists
                             Console.WriteLine("Fail to insert item."
                                 + "\nPlease re-enter item or enter '0' to return to main menu:");
                         if (item != 0)
-                        {
-                            S.Insert(item);
-                            S.Print();
-                            S.Profile();
-                        }
+                        { S.Insert(item); S.Print(); S.Profile();}
                         break;
-
                     // call Remove()
                     case 'D':
                         Console.Write("Please enter the item you would like to remove >> ");
@@ -401,13 +354,8 @@ namespace SkipLists
                             Console.WriteLine("Input not valid."
                                 + "\nPlease re-enter item or enter '0' to return to main menu:");
                         if (item != 0)
-                        {
-                            S.Remove(item);
-                            S.Print();
-                            S.Profile();
-                        }
+                        { S.Remove(item); S.Print(); S.Profile(); }
                         break;
-
                     // call Contains()
                     case 'F':
                         Console.Write("Please enter the item >> ");
@@ -416,15 +364,15 @@ namespace SkipLists
                                 + "\nPlease re-enter item or enter '0' to return to main menu:");
                         if (item != 0)
                         {
-                            if (S.Contains(item))
+                            if (S.Contains(item)) // if found
                                 Console.WriteLine(item + " found at rank " + S.RankII(item));
-                            else
+                            else // if not found, inform user
                                 Console.WriteLine(item + " not found");
                         }
                         break;
-
                     // call RankI()
                     case 'I':
+                        // remind user of current existing items
                         Console.WriteLine("Current items in the Skip List are: ");
                         S.Print();
                         Console.Write("Please enter the Rank >> ");
@@ -432,14 +380,14 @@ namespace SkipLists
                             Console.WriteLine("Input not valid."
                                 + "\nPlease re-enter item or enter '0' to return to main menu:");
                         int rankedItem = S.RankI(item);
-                        if (rankedItem != 0)
+                        if (rankedItem != 0) // if found
                             Console.WriteLine("The item of rank " + item + " is " + rankedItem);
-                        else
+                        else // if input out of bounds, inform user
                             Console.WriteLine("The rank entered is out of bounds of the skip list");
                         break;
-
                     // call RankII()
                     case 'R':
+                        // remind user of current existing items
                         Console.WriteLine("Current items in the Skip List are: ");
                         S.Print();
                         Console.Write("Please enter the item you would like to be ranked >> ");
@@ -447,28 +395,22 @@ namespace SkipLists
                             Console.WriteLine("Input not valid."
                                 + "\nPlease re-enter item or enter '0' to return to main menu:");
                         int rank = S.RankII(item);
-                        if (rank > 0)
+                        if (rank > 0) // successfull Rank II operation (item found)
                             Console.WriteLine("The rank of " + item + " is " + rank);
-                        else
+                        else // item not found
                             Console.WriteLine(item + " is not found in the Skip List.");
                         break;
-
                     // stop program
                     case 'Q': //quit
                         run = false; //main loop control variable
                         break;
-
-
                     default: //if menu input was a char but didn't match up with any valid menu operations
                         Console.WriteLine("Unrecognised input, please try again");
                         break;
-                }
-
-                System.Threading.Thread.Sleep(1000);
+                } System.Threading.Thread.Sleep(1000);
             }
-
             Console.WriteLine("\nThank you for using the Skip List. Goodbye!");
-            Console.ReadLine();
+            Console.ReadLine(); // hold output window
         }
     }
 }
